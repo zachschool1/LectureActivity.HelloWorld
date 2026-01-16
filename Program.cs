97c63d6b2b12ddc37efd5253165c4069aedae2f2
zachschool1 <zacharyjordan@my.whccd.edu>
2026-01-15 21:46:54 -0800

    i wrote a for loop

--- Program.cs
+++ Program.cs
@@ -6,6 +6,12 @@
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("This is a small change.");
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
